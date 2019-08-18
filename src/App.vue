a0e2ccb72d0f178f9c30629d9e0ee77e26fca02b
diehlms <diehlstx@gmail.com>
2019-08-17 19:37:39 -0500

    overhauled old app

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,73 @@
 <template>
   <div id="app">
-    <h1 class="ui dividing centered header">Vue.js Todo App</h1>
-    <div class='ui three column centered grid'>
-      <div class='column'>
-        <todo-list v-bind:todos="todos"></todo-list>
-        <create-todo v-on:create-todo="createTodo"></create-todo>
-      </div>
-    </div>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import TodoList from './components/TodoList';
-import CreateTodo from './components/CreateTodo';
+
+import Todos from './components/Todos.vue';
+import Header from './components/layout/Header.vue'
+import AddTodo from './components/AddTodo.vue';
+import axios from 'axios'
+
 export default {
   name: 'app',
   components: {
-    TodoList,
-    CreateTodo,
+    Todos,
+    Header,
+    AddTodo,
   },
   data() {
     return {
-      todos: [{
-        title: 'Todo A',
-        project: 'Project A',
-        done: false,
-      }, {
-        title: 'Todo B',
-        project: 'Project B',
-        done: true,
-      }, {
-        title: 'Todo C',
-        project: 'Project C',
-        done: false,
-      }, {
-        title: 'Todo D',
-        project: 'Project D',
-        done: false,
-      }],
-    };
+      todos: []
+    }
   },
   methods: {
-    createTodo(newTodo) {
-      this.todos.push(newTodo);
-      sweetalert('Success!', 'To-Do created!', 'success');
+    deleteTodo(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err => console.log(err));
     },
+    addTodo(newTodo) {
+      const {title, completed} = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+      .then(res => this.todos = [...this.todos, newTodo])
+      .catch(err => console.log(err));
+    } 
   },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(res => this.todos = res.data)
+    .catch(err => console.log(err))
+  }
 };
 </script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+</style>
